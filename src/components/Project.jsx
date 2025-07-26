7ef1a46e14aede326c46f090214cd415d2027abc
nikhil <nikhilsharma270027@gmail.com>
2025-07-26 16:25:52 +0530

    changed background , removed birder of home section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
         <ul className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 max-lg:grid-col gap-y-8 gap-x-4 overflow-hidden">
           {ProjectTemplate.map((project, index) => (
             <FadeIn key={index}>
-              <li className="bg-gray-900 text-white rounded-xl p-5 shadow-lg   relative overflow-hidden border border-transparent hover:border-neon-blue shadow-neon">
+              <li className="bg-gray-900 text-white rounded-xl p-5 shadow-lg relative overflow-hidden border border-transparent hover:border-neon-blue ">
                 {/* Neon glow effect */}
                 <div className="absolute inset-0 -z-10 bg-gradient-to-r from-transparent via-neon-blue/20 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-300"></div>
                 <div className="absolute inset-0 -z-10 neon-glow"></div>
@@ -26,7 +26,7 @@ const Projects = () => {
                 <p className="text-sm text-yellow-400 mb-1">
                   {index !== 0 && project.type === "Serious Project" ? "⚡ Serious Project" : "✨ Fun Project"}
                 </p>
-                <h3 className="text-xl font-semibold mb-1 text-yellow-400 hover:text-shadow-neon-pink-hover">{project.title}</h3>
+                <h3 className="text-xl font-semibold mb-1 text-yellow-400 hover:shadow-neon-pink-hover">{project.title}</h3>
                 <p className="text-sm text-gray-300 mb-3">{project.description}</p>
 
                 <p className="font-medium mb-1 text-green-400">Features:</p>
@@ -45,7 +45,7 @@ const Projects = () => {
 
                 <a
                   href={project.link}
-                  className="text-sm text-blue-500 hover:text-neon-blue hover:underline text-shadow-neon"
+                  className="text-sm text-blue-500 hover:text-yellow-500 hover:underline"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
